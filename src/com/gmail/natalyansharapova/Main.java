86839cd15438ffee3b1f660e216ea1d19da33ba7
sharapovaNn <natalya.n.sharapova@gmail.com>
2022-04-05 19:41:58 +0300

    !

--- src/com/gmail/natalyansharapova/Main.java
+++ src/com/gmail/natalyansharapova/Main.java
@@ -3,7 +3,7 @@ package com.gmail.natalyansharapova;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello");
 	// write your code here
     }
 }
