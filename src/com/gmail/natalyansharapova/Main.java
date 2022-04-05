802bc8e2ae23312e02e1724e5afb56aaefe7425c
sharapovaNn <natalya.n.sharapova@gmail.com>
2022-04-05 19:20:03 +0300

    add hello

---
+++ src/com/gmail/natalyansharapova/Main.java
@@ -0,0 +1,9 @@
+package com.gmail.natalyansharapova;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+	// write your code here
+    }
+}
